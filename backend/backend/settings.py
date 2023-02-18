f47ea72d8b3fe90b8c034a47ccb9600bd6d082ea
Maxim Solodilov <sldmxm@gmail.com>
2023-02-18 17:51:38 +0700

    after sixth review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-Database
-https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -93,7 +93,6 @@ DATABASES = {
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'mydatabase',
 #     }
 # }
 
